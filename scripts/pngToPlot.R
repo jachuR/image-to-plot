85987e4c364f01647ea13d1ad92658ff180de83d
jachuR <jatoritter@gmail.com>
2020-02-24 16:10:34 +0100

    auto clusters

--- scripts/pngToPlot.R
+++ scripts/pngToPlot.R
@@ -11,12 +11,13 @@ pacman::p_load(imager, dplyr ,plotly, tidyr, ggplot2)
 
 
 # inputs: file_path - .jpg, .png or other acceptable by imager
-#          colours_n - number of color clusters
+#         colours_n - number of color clusters - if "auto" - NbClust determine the best number of clusters.
 #         algorithm - algorithm for kmeans fuction. One from the list. 
-#         sample_size - number of point for output plot  
+#         sample_size - number of point for output plot
+#         index	- the index to be calculated by NbClust,  "all" - all indices
 # output of the function is ggplot with transformed image
 
-pngToPlot  <- function(file_path, colours_n, algorithm = c("Hartigan-Wong","Lloyd", "Forgy", "MacQueen")
+pngToPlot  <- function(file_path, colours_n = 'auto', algorithm = c("Hartigan-Wong","Lloyd", "Forgy", "MacQueen"), index = 'sdindex'
                        ,sample_size =10000) {
   img <- load.image(file_path) #load data as cimg
   
@@ -33,10 +34,24 @@ pngToPlot  <- function(file_path, colours_n, algorithm = c("Hartigan-Wong","Lloy
     hex
     })
 
-## using kmeans to reduce the number of colors to the desired amount -------
-
+## reduce the number of colors to the desired amount -------
+  
+  if (colours_n == "auto") { # determining the best number of clusters with sdindex
+    set.seed(123)
+    nb <- NbClust(sample_n(img_DT_chan_dcast[3:5],5000), distance = "euclidean", min.nc = 2, 
+                  max.nc = 10, method = "complete", index =index) #index = sdindex - gives nice results for flags, change for "all" if you want to check 26 different indices
+    best_nc <- nb$Best.nc
+    best_nc <- as.data.frame(t(best_nc))
+    best_nc$Number_clusters <- as.factor(best_nc$Number_clusters)
+    ss <- summary(best_nc$Number_clusters)
+    colours_n <- as.numeric(names(which.max(ss))) # most frequently proposed number of clusters (if you use index = "all")
+  }
+    
+    
+  set.seed(123)
   clusters <-  kmeans(img_DT_chan_dcast[c('R','G','B')],centers =  colours_n,iter.max = 50,algorithm = algorithm,nstart = 3)
   img_clusters <- data.frame(img_DT_chan_dcast, cluster = as.character (clusters$cluster))
+  
   
   kColours <- rgb(clusters$centers[clusters$cluster,]) #define  the color of each cluster based on its center parameters
   img_clusters <- data.frame(img_clusters, kColour = kColours) # add colour information to data frame
@@ -84,7 +99,7 @@ pngToPlot  <- function(file_path, colours_n, algorithm = c("Hartigan-Wong","Lloy
 
 # Exampel of use ---------------------------------------------------------
 
-pngToPlot(file_path = "images/norway.png", colours_n = 3, sample_size = 15000,algorithm = "MacQueen")
+pngToPlot(file_path = "flag-of-ghana.png", colours_n = 4, sample_size = 11000,algorithm = "MacQueen")
 
 
 
